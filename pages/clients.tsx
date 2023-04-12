ce36451de11fc5dcebb33695fe189db6b1ef77c1
Marlon Costa <mrlnrnn@gmail.com>
2023-04-11 23:52:20 -0300

    Algumas pequenas refatorações e início da lógica de salvamento

--- pages/clients.tsx
+++ pages/clients.tsx
@@ -1,12 +1,44 @@
-import { useEffect, useState } from 'react';
-import { VStack, Text, Divider, Input, InputGroup, InputLeftElement, Icon, Modal, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalOverlay, Box, Flex, Button, GridItem, Grid, ModalFooter, CircularProgress, Center } from '@chakra-ui/react';
-import { SearchIcon } from '@chakra-ui/icons';
-import { FaDollarSign, FaIdCard, FaInstagram, FaMapMarkerAlt, FaMoneyBill, FaPhone, FaShoppingCart } from 'react-icons/fa';
-import { Database } from '@/lib/database.types';
-import { supabase } from '../lib/supabase';
+import {useEffect, useState} from 'react';
+import {
+    VStack,
+    Text,
+    Divider,
+    Input,
+    InputGroup,
+    InputLeftElement,
+    Icon,
+    Modal,
+    ModalContent,
+    ModalHeader,
+    ModalCloseButton,
+    ModalBody,
+    ModalOverlay,
+    Box,
+    Flex,
+    Button,
+    GridItem,
+    Grid,
+    ModalFooter,
+    CircularProgress,
+    Center
+} from '@chakra-ui/react';
+import {SearchIcon} from '@chakra-ui/icons';
+import {
+    FaDollarSign,
+    FaIdCard,
+    FaInstagram,
+    FaMapMarkerAlt,
+    FaMoneyBill,
+    FaPhone,
+    FaShoppingCart
+} from 'react-icons/fa';
+import {Database} from '@/lib/database.types';
+import {supabase} from '@/lib/supabase';
 
 
 type Client = Database['public']['Tables']['clientes']['Row'];
+type ClientInsert = Database['public']['Tables']['clientes']['Insert'];
+type ClientUpdate = Database['public']['Tables']['clientes']['Update'];
 
 // This is a sample data of clients. You can fetch data from your backend API.
 
@@ -14,9 +46,10 @@ interface ClientCardProps {
     client: Client | null;
     isOpen: boolean;
     onClose: () => void;
+    onSave: (client: ClientUpdate) => void;
 }
 
-function ClientCard({ client, isOpen, onClose }: ClientCardProps) {
+function ClientCard({client, isOpen, onClose, onSave}: ClientCardProps) {
     const [isEditing, setIsEditing] = useState(false);
 
     if (!client) {
@@ -27,115 +60,121 @@ function ClientCard({ client, isOpen, onClose }: ClientCardProps) {
         setIsEditing((prevIsEditing) => !prevIsEditing);
     };
 
-    const handleSaveClick = () => {
-        // TODO: Implement save logic
-        setIsEditing(false);
-    };
-
     return (
         <Modal isOpen={isOpen} onClose={onClose} size="2xl">
-            <ModalOverlay />
+            <ModalOverlay/>
             <ModalContent>
                 <ModalHeader bg="pink.400" color="white">
                     {client.nome} {client.sobrenome} ({client.apelido})
                 </ModalHeader>
-                <ModalCloseButton color='white' />
+                <ModalCloseButton color='white'/>
                 <ModalBody>
                     <VStack align="flex-start">
                         <Grid templateRows='repeat(4, 1fr)'
-                            templateColumns='repeat(6, 1fr)' gap={2}>
+                              templateColumns='repeat(6, 1fr)' gap={2}>
 
                             <GridItem colSpan={2}>
                                 <Text>Nome:</Text>
-                                <Input defaultValue={client.nome} disabled={!isEditing} _disabled={{ color: 'black' }} />
+                                <Input defaultValue={client.nome} disabled={!isEditing} _disabled={{color: 'black'}}/>
                             </GridItem>
 
                             <GridItem colSpan={2}>
                                 <Text>Sobrenome:</Text>
-                                <Input defaultValue={client.sobrenome} disabled={!isEditing} _disabled={{ color: 'black' }} />
+                                <Input defaultValue={client.sobrenome} disabled={!isEditing}
+                                       _disabled={{color: 'black'}}/>
                             </GridItem>
 
                             <GridItem colSpan={2}>
                                 <Text>Apelido:</Text>
-                                <Input defaultValue={client.apelido ?? ""} disabled={!isEditing} _disabled={{ color: 'black' }} />
+                                <Input defaultValue={client.apelido ?? ""} disabled={!isEditing}
+                                       _disabled={{color: 'black'}}/>
                             </GridItem>
 
                             <GridItem colSpan={2}>
                                 <Text>CPF:</Text>
                                 <InputGroup>
-                                    <InputLeftElement pointerEvents='none' children={<FaIdCard color='#ED64A6' />} />
-                                    <Input defaultValue={client.cpf} disabled={!isEditing} _disabled={{ color: 'black' }} />
+                                    <InputLeftElement pointerEvents='none'><FaIdCard
+                                        color='#ED64A6'/></InputLeftElement>
+                                    <Input defaultValue={client.cpf} disabled={!isEditing}
+                                           _disabled={{color: 'black'}}/>
                                 </InputGroup>
                             </GridItem>
 
                             <GridItem colSpan={2}>
                                 <Text>Telefone:</Text>
                                 <InputGroup>
-                                    <InputLeftElement
-                                        pointerEvents='none'
-                                        children={<FaPhone color='#ED64A6' />}
-                                    />
-                                    <Input defaultValue={client.telefone} disabled={!isEditing} _disabled={{ color: 'black' }} />
+                                    <InputLeftElement pointerEvents='none'>
+                                        <FaPhone color='#ED64A6'/></InputLeftElement>
+                                    <Input defaultValue={client.telefone} disabled={!isEditing} _disabled={{color: 'black'}}/>
                                 </InputGroup>
                             </GridItem>
 
                             <GridItem colSpan={2}>
                                 <Text>Instagram:</Text>
                                 <InputGroup>
-                                    <InputLeftElement pointerEvents='none' children={<FaInstagram color='#ED64A6' />} />
+                                    <InputLeftElement pointerEvents='none'><FaInstagram
+                                        color='#ED64A6'/></InputLeftElement>
 
-                                    <Input defaultValue={client.instagram ?? ""} disabled={!isEditing} _disabled={{ color: 'black' }} />
+                                    <Input defaultValue={client.instagram ?? ""} disabled={!isEditing}
+                                           _disabled={{color: 'black'}}/>
                                 </InputGroup>
                             </GridItem>
 
                             <GridItem colSpan={4}>
                                 <Text>Endereço:</Text>
                                 <InputGroup>
-                                    <InputLeftElement pointerEvents='none' children={<FaMapMarkerAlt color='#ED64A6' />} />
-                                    <Input defaultValue={client.endereco} disabled={!isEditing} _disabled={{ color: 'black' }} />
+                                    <InputLeftElement pointerEvents='none'
+                                    ><FaMapMarkerAlt color='#ED64A6'/></InputLeftElement>
+                                    <Input defaultValue={client.endereco} disabled={!isEditing}
+                                           _disabled={{color: 'black'}}/>
                                 </InputGroup>
                             </GridItem>
 
                             <GridItem colSpan={2}>
                                 <Text>Número:</Text>
-                                <Input defaultValue={client.numero_endereco} disabled={!isEditing} _disabled={{ color: 'black' }} />
+                                <Input defaultValue={client.numero_endereco} disabled={!isEditing}
+                                       _disabled={{color: 'black'}}/>
                             </GridItem>
 
                             <GridItem colSpan={2}>
                                 <Text>Saldo Devedor:</Text>
                                 <InputGroup>
-                                    <InputLeftElement pointerEvents='none' children={<FaDollarSign color='#ED64A6' />} />
-                                    <Input defaultValue={'R$ ' + client.saldo_devedor.toFixed(2) ?? ""} disabled={true} _disabled={{ color: 'black' }} />
+                                    <InputLeftElement pointerEvents='none'><FaDollarSign
+                                        color='#ED64A6'/></InputLeftElement>
+                                    <Input defaultValue={'R$ ' + client.saldo_devedor.toFixed(2) ?? ""} disabled={true}
+                                           _disabled={{color: 'black'}}/>
                                 </InputGroup>
                             </GridItem>
 
                             <GridItem colSpan={2}>
                                 <Text>Última Compra:</Text>
                                 <InputGroup>
-                                    <InputLeftElement pointerEvents='none' children={<FaShoppingCart color='#ED64A6' />} />
-                                    <Input defaultValue={client.ultima_compra ?? ""} disabled={true} _disabled={{ color: 'black' }} />
+                                    <InputLeftElement pointerEvents='none'
+                                    ><FaShoppingCart color='#ED64A6'/></InputLeftElement>
+                                    <Input defaultValue={client.ultima_compra ?? ""} disabled={true}
+                                           _disabled={{color: 'black'}}/>
                                 </InputGroup>
                             </GridItem>
 
                             <GridItem colSpan={2}>
                                 <Text>Último Pagamento:</Text>
                                 <InputGroup>
-                                    <InputLeftElement pointerEvents='none' children={<FaMoneyBill color='#ED64A6' />} />
-                                    <Input defaultValue={client.ultimo_pagamento ?? ""} disabled={true} _disabled={{ color: 'black' }} />
+                                    <InputLeftElement pointerEvents='none'><FaMoneyBill
+                                        color='#ED64A6'/></InputLeftElement>
+                                    <Input defaultValue={client.ultimo_pagamento ?? ""} disabled={true}
+                                           _disabled={{color: 'black'}}/>
                                 </InputGroup>
                             </GridItem>
-
                         </Grid>
-
-
                     </VStack>
                 </ModalBody>
                 <ModalFooter>
-                    <Button onClick={handleToggleEdit} _hover={{ bg: isEditing ? 'red.500' : 'pink.400', color: 'white' }}>
+                    <Button onClick={handleToggleEdit}
+                            _hover={{bg: isEditing ? 'red.500' : 'pink.400', color: 'white'}}>
                         {isEditing ? 'Cancelar' : 'Editar'}
                     </Button>
                     {isEditing && (
-                        <Button ml={3} colorScheme="green" onClick={handleSaveClick}>
+                        <Button ml={3} colorScheme="green" onClick={() => onSave(client)}>
                             Salvar
                         </Button>
                     )}
@@ -156,7 +195,7 @@ function ClientPage() {
         const fetchClients = async () => {
             setIsLoading(true);
             try {
-                let { data: clients, error } = await supabase
+                let {data: clients, error} = await supabase
                     .from('clientes')
                     .select('*')
                 if (error) {
@@ -169,8 +208,34 @@ function ClientPage() {
                 console.error(err);
             }
         }
-        fetchClients();
+        fetchClients().then(response => console.log(response));
     }, []);
+
+    const handleSaveClick = async (client: ClientUpdate) => {
+        console.log("Saving client: " + client.nome)
+        const {data: updatedClient, error} = await supabase
+            .from("clientes")
+            .update(
+                {
+                    nome: client.nome,
+                    sobrenome: client.sobrenome,
+                    apelido: client.apelido,
+                    cpf: client.cpf,
+                    telefone: client.telefone,
+                    instagram: client.instagram,
+                    endereco: client.endereco,
+                    numero_endereco: client.numero_endereco,
+                }
+            )
+            .eq("id", client.id);
+
+        if (error) {
+            console.error(error);
+            return;
+        }
+
+        console.log(updatedClient);
+    };
 
 
     const onClientCardOpen = (client: Client) => {
@@ -190,7 +255,7 @@ function ClientPage() {
     if (isLoading) {
         return (
             <Center verticalAlign="center" h="100vh">
-                <CircularProgress isIndeterminate color="pink.400" />
+                <CircularProgress isIndeterminate color="pink.400"/>
             </Center>
         )
     }
@@ -198,23 +263,26 @@ function ClientPage() {
     return (
         <VStack spacing="4" p="4">
             <InputGroup>
-                <InputLeftElement pointerEvents="none" >
-                    <Icon as={SearchIcon} color="gray.300" />
+                <InputLeftElement pointerEvents="none">
+                    <Icon as={SearchIcon} color="gray.300"/>
                 </InputLeftElement>
-                <Input type="text" placeholder="Busca clientes por nome, sobrenome ou apelido" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} focusBorderColor="pink.400" />
+                <Input type="text" placeholder="Busca clientes por nome, sobrenome ou apelido" value={searchTerm}
+                       onChange={(e) => setSearchTerm(e.target.value)} focusBorderColor="pink.400"/>
             </InputGroup>
-            <Divider />s
+            <Divider/>
             <VStack w="100%">
                 {filteredClients.map(client => (
-                    <Box key={client.cpf} w="100%" >
-                        <Flex w="100%" p="2" borderRadius="md" bg="gray.100" _hover={{ bg: 'pink.400', color: 'white', cursor: 'pointer' }} onClick={() => onClientCardOpen(client)}>
-                        <Text fontSize="lg" fontWeight="bold">{`${client.nome} ${client.sobrenome}${client.apelido ? ` (${client.apelido})` : ''}`}</Text>
-
+                    <Box key={client.cpf} w="100%">
+                        <Flex w="100%" p="2" borderRadius="md" bg="gray.100"
+                              _hover={{bg: 'pink.400', color: 'white', cursor: 'pointer'}}
+                              onClick={() => onClientCardOpen(client)}>
+                            <Text fontSize="lg"
+                                  fontWeight="bold">{`${client.nome} ${client.sobrenome}${client.apelido ?? ''}`}</Text>
                         </Flex>
                     </Box>
                 ))}
             </VStack>
-            <ClientCard isOpen={isClientCardOpen} onClose={onClientCardClose} client={selectedClient} />
+            <ClientCard isOpen={isClientCardOpen} onClose={onClientCardClose} client={selectedClient} onSave={handleSaveClick}/>
         </VStack>
     );
 }
