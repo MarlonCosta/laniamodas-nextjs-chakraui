d76047bf3206cb4fe64f53d8c31872af40e1696e
Marlon Costa <mrlnrnn@gmail.com>
2023-05-09 00:36:00 -0300

    Sales Page

--- pages/sales.tsx
+++ pages/sales.tsx
@@ -1,7 +1,33 @@
+import {Table, TableContainer, Tbody, Td, Th, Thead, Tr, VStack} from "@chakra-ui/react";
+import React from "react";
+
 export default function SalesPage() {
     return (
-        <div>
-            <h1>Sales</h1>
-        </div>
+        <VStack>
+            <TableContainer>
+                <Table>
+                    <Thead bg={"pink.400"}>
+                        <Tr bg={"pink.400"} sx={{"&:hover": {background: "none", color: "inherit"}}}>
+                            <Th key="codigo">Cód. Venda</Th>
+                            <Th key="cliente">Cliente</Th>
+                            <Th key="data">Data</Th>
+                            <Th key="valor">Valor</Th>
+                            <Th key="desconto">Desconto</Th>
+                            <Th key="total">Total</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        <Tr>
+                            <Td>1</Td>
+                            <Td>Marlon</Td>
+                            <Td>28/02/1993</Td>
+                            <Td>R$ 100.00</Td>
+                            <Td>15%</Td>
+                            <Td>R$ 85.00</Td>
+                        </Tr>
+                    </Tbody>
+                </Table>
+            </TableContainer>
+        </VStack>
     )
 }
